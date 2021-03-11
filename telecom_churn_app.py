66e81ee6d91728a6ee9b789579aeda7ca50d716c
Projeet <projeet25july@gmail.com>
2021-03-11 06:30:30 -0800

    Add files via upload

---
+++ telecom_churn_app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template
+import numpy as np 
+import joblib
+
+
+app  = Flask(__name__)
+
+model1 = joblib.load('model/logistic_model.pkl')
+
+@app.route('/')
+def home():
+	return render_template('front_page.html')
+
+@app.route('/predict', methods = ['POST','GET'])
+def telecom_churn_prediction():
+	if (request.method == 'POST'):
+		int_features = [x for x in request.form.values()]
+		final_features = [np.array(int_features)]
+		final_features = np.asarray(final_features, dtype='float64')
+		output = model1.predict(final_features)
+		if output == 0:
+			return render_template('index.html', prediction_text='Customer will not churn')
+		else:
+			return render_template('index.html', prediction_text='Churn will churn')	
+	else :
+		return render_template('index.html')
+
+
+
+
+
+
+if __name__ == "__main__":
+    # Start Application
+    app.run(debug=True)
